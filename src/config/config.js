586e0d021f5ea5af6766661a35fa84f2a6201af9
Stephen C <61793361+stephen-cx@users.noreply.github.com>
2020-07-02 17:42:16 +0100

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,9 @@
 // Placeholder file
 module.exports = {
     database: {
-        url: "mongodb+srv://steve:FlJvYsMDAllQAYGx@mongo-20-girxd.mongodb.net/irish-heritage-sites?retryWrites=true&w=majority",
-        password: "FlJvYsMDAllQAYGx",
-        username: "steve",
+        url: "mongodb+srv://<username>:<password>@mongo-20-girxd.mongodb.net/irish-heritage-sites?retryWrites=true&w=majority",
+        password: "placeholder",
+        username: "placeholder",
         dbName: "irish-heritage-sites"
     }
 }
